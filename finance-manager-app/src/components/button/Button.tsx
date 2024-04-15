bff271259daef18261db9cf7d9a15499f4e9bf01
leandromacedo117 <leeandromacedo117@gmail.com>
2024-04-15 13:22:49 -0300

    back-end

--- finance-manager-app/src/components/button/Button.tsx
+++ finance-manager-app/src/components/button/Button.tsx
@@ -1,26 +1,21 @@
-import React from 'react'
-import './Button.css'
+import React from 'react';
+import './Button.css';
 
-type nameProps = {
-    nameProps : string;
+type ButtonProps = {
+    nameProps: string;
+    onSubmit?: () => void; // Adicionando propriedade opcional para onClick
 }
 
-const sendData = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    
-}
-
-const Button = ({nameProps}: nameProps) => {
-  return (
-    <>
-        <button 
-        type="submit"
-        onSubmit={sendData}
-        className='Button-components'
-        > {nameProps}
+const Button: React.FC<ButtonProps> = ({ nameProps, onSubmit: onSubmit }) => {
+    return (
+        <button
+            type="submit"
+            className='Button-components'
+            onSubmit={onSubmit} // Utilize onClick em vez de onSubmit
+        >
+            {nameProps}
         </button>
-    </>
-  )
+    );
 }
 
-export default Button
+export default Button;
---
+++ finance-manager-app/src/hooks/useFetch.tsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// function useFetch<T>(url: RequestInfo | URL, options?: RequestInit)() {
+//     const [ data, setData] = React.useState(null);
+//     const [loading, set]
+
+// }
+
+// export default useFetch
